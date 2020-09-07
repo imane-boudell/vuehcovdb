8f3340be21bd2e0d900bdbeb2534ca6528401a5b
Imane Boudell <imane@Imanes-MacBook-Pro.local>
2020-09-07 22:24:54 +0300

    Adding email icons..

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,15 @@
   <div class="container">
     <h1>Contact Us</h1><br>
     <h4><i class="fa fa-user"></i>&nbsp;<b>Imane Boudellioua, PhD.</b><br/>
-    <i class="fa fa-envelope"></i>&nbsp;<b>imane.boudellioua@kfupm.edu.sa</b><br/>
-    <i>Assistant Professor</i><br/>
+    <i class="fa fa-envelope"></i>&nbsp;<b>iboudellioua@hcoronaviruses.org</b><br/>
+    <img src="../assets/email-imane.png" style="width:400px;height:auto;"><br>    <i>Assistant Professor</i><br/>
     Information and Computer Science Department, <br/>
     King Fahd University of Petroleum and Minerals </h4>
    <br/><br/>
    
     <h4><i class="fa fa-user"></i>&nbsp;<b>Iman Almansour, PhD.</b><br/>
-    <i class="fa fa-envelope"></i>&nbsp;<b>ikalmansour@iau.edu.sa</b><br/>
-    <i>Assistant Professor</i><br/>
+    <i class="fa fa-envelope"></i>&nbsp;<b>ialmansour@hcoronaviruses.org</b><br/>
+    <img src="../assets/email-iman.png" style="width:330px;height:auto;"><br>    <i>Assistant Professor</i><br/>
     Epidemic Diseases Department, Institute for Research and Medical Consultations <br/>
     Imam Abdulrahman Bin Faisal University </h4>
    
